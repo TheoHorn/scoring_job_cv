e7ff54261ac2e5ad1ef2d60502e083eb04911e0d
TheoHorn <82098718+TheoHorn@users.noreply.github.com>
2024-11-18 12:12:12 +0100

    [FEAT] cv parser v0

---
+++ src/tri/txt2data.py
@@ -0,0 +1,24 @@
+# Import libraries
+
+# Import txt from csv
+
+# Transform txt to data
+
+## Read txt file
+
+## Find the key words (e.g. "Name", "Skills", "Experience", etc.)
+
+## Extract each key word and its content
+
+## Store the content in a dictionary
+
+### for each content
+
+### remove stop words
+
+### remove special characters
+
+### stem the words
+
+### dico to CSV
+
